8ebbed7624a54fef09d368b2a1a7bca4ff20cccf
taka01150810 <taka01150810@gmail.com>
2024-06-30 13:18:37 +0900

    リアクティブなテンプレート変数の基本のref() 計算結果をリアクティブとするcomputed()

--- sfc-basic/app.vue
+++ sfc-basic/app.vue
@@ -1,6 +1,25 @@
+<script setup lang="ts">
+// 乱数を取得
+const widthInit = Math.round(Math.random() * 10);
+const heightInit = Math.round(Math.random() * 10);
+
+// リアクティブ変数 ref
+const width = ref(widthInit);
+const height = ref(heightInit);
+
+// refによって用意したリアクティブな変数の値を変更するときは.valueプロパティにアクセスする
+// computedは算出プロパティ。リアクティブな変数をもとに変数を設定する
+const area = computed((): number => {
+  return width.value * height.value;
+});
+
+if (typeof window !== "undefined") {
+  setInterval((): void => {
+    width.value = Math.round(Math.random() * 10);
+    height.value = Math.round(Math.random() * 10);
+  }, 10000);
+}
+</script>
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <p>縦{{ height }}で横が{{ width }}の長方形の面積は{{ area }}</p>
 </template>
