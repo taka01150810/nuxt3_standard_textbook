360f69c805bb9bda079b89eb6ec1cec7d637f2f0
taka01150810 <taka01150810@gmail.com>
2024-06-30 13:26:02 +0900

    オブジェクトをまとめてリアクティブにできるreactive()

--- sfc-basic/app.vue
+++ sfc-basic/app.vue
@@ -6,23 +6,30 @@
 const widthInit = Math.round(Math.random() * 10);
 const heightInit = Math.round(Math.random() * 10);
 
-// リアクティブ変数 ref
-const width = ref(widthInit);
-const height = ref(heightInit);
+// オブジェクトをまとめてリアクティブにできるreactive()
+const rectangle = reactive({
+  width: widthInit,
+  height: heightInit,
+});
+// Vueではref()とreactive()の二種類のメソッドが利用できる。
+// 使い分けとしては、まずref()をおすすめ。理由はその方がコードの見通しが良くなる
 
-// refによって用意したリアクティブな変数の値を変更するときは.valueプロパティにアクセスする
 // computedは算出プロパティ。リアクティブな変数をもとに変数を設定する
 const area = computed((): number => {
-  return width.value * height.value;
+  return rectangle.width * rectangle.height;
 });
 
 if (typeof window !== "undefined") {
   setInterval((): void => {
-    width.value = Math.round(Math.random() * 10);
-    height.value = Math.round(Math.random() * 10);
+    rectangle.width = Math.round(Math.random() * 10);
+    rectangle.height = Math.round(Math.random() * 10);
   }, 10000);
 }
 </script>
 <template>
-  <p>縦{{ height }}で横が{{ width }}の長方形の面積は{{ area }}</p>
+  <p>
+    縦{{ rectangle..height }}で横が{{ rectangle.width }}の長方形の面積は{{
+      area
+    }}
+  </p>
 </template>
