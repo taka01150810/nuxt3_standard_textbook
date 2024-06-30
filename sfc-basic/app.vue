703fc9ef0d503eb8398afbebc7f22e254aee897e
taka01150810 <taka01150810@gmail.com>
2024-06-30 13:38:10 +0900

    引数なし

--- sfc-basic/app.vue
+++ sfc-basic/app.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 const msg = ref("まだ");
-const onButtonClick = (label: string, event: Event): void => {
-  const target = event.target as HTMLButtonElement;
-  const text = target.innerText;
-  msg.value = `${label}と${text}`;
+const onButtonClick = (): void => {
+  msg.value = "引数なしでクリック";
 };
 </script>
 
 <template>
   <p>{{ msg }}</p>
-  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
+  <button v-on:click="onButtonClick">こんにちは</button>
 </template>
